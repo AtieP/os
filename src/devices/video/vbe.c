7c3f3d2604d0830d904a5a601115215b2b5290fc
AtieP <AtieP@users.noreply.github.com>
2020-10-30 08:37:23 +0100

    Fix scrolling and printing

--- src/devices/video/vbe.c
+++ src/devices/video/vbe.c
@@ -34,17 +34,17 @@ void OsDriver_Video_VBE_Putc(const char ch, uint32_t color) {
 	if (ch == '\n') {
 		vbe_cursor_x = 0;
 		vbe_cursor_y++;
-		return;
-	}
-	uint8_t *a = framebuffer.font_address + (uint16_t) ch * ((framebuffer.font_width / 8) * framebuffer.font_height);
-	for (int i = 0; i < framebuffer.font_height; i++) {
-		for (int j = 0, bit = framebuffer.font_width; j < framebuffer.font_width; j++) {
-			if (a[i] & (1 << --bit)) {
-				framebuffer.address[(i + (vbe_cursor_y * framebuffer.font_height)) * framebuffer.pitch + j + (vbe_cursor_x * framebuffer.font_width)] = color;
+	} else {
+		uint8_t *a = framebuffer.font_address + (uint16_t) ch * ((framebuffer.font_width / 8) * framebuffer.font_height);
+		for (int i = 0; i < framebuffer.font_height; i++) {
+			for (int j = 0, bit = framebuffer.font_width; j < framebuffer.font_width; j++) {
+				if (a[i] & (1 << --bit)) {
+					framebuffer.address[(i + (vbe_cursor_y * framebuffer.font_height)) * framebuffer.pitch + j + (vbe_cursor_x * framebuffer.font_width)] = color;
+				}
 			}
 		}
+		vbe_cursor_x++;
 	}
-	vbe_cursor_x++;
 	if (vbe_cursor_x == framebuffer.max_chars_width) {
 		vbe_cursor_x = 0;
 		vbe_cursor_y++;
@@ -64,7 +64,7 @@ void OsDriver_Video_VBE_Puts(const char *string, uint32_t color) {
 
 void OsDriver_Video_VBE_Scroll(void) {
 	uint32_t row_size = framebuffer.pitch * framebuffer.font_height;
-	uint32_t screen_size = framebuffer.pitch = framebuffer.max_chars_height * framebuffer.font_height;
+	uint32_t screen_size = framebuffer.pitch * framebuffer.max_chars_height * framebuffer.font_height;
 	// The actual move
 	for (int i = 0; i < screen_size - row_size; i++) {
 		framebuffer.address[i] = framebuffer.address[i + row_size];
